4536c31c42134db99b70343155e8868a04d69eae
gdut-yy <gdut_yy@163.com>
2020-01-27 19:43:01 +0800

    vuepress 国际化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,76 +1,132 @@
 // .vuepress/config.js
 module.exports = {
-  // 网站的标题
-  title: "Clean Architecture 中文",
-  // 上下文根
   base: "/doc-cleanarch/",
-  themeConfig: {
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: "gdut-yy/Clean-Architecture-zh",
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: "Github",
-    // 以下为可选的编辑链接选项
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    docsRepo: "gdut-yy/Clean-Architecture-zh",
-    // 假如文档放在一个特定的分支下：
-    docsBranch: "master/docs",
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: "帮助我们改善此页面！",
-    // 最后更新时间
-    lastUpdated: "Last Updated",
-    // 最大深度
-    sidebarDepth: 2,
-    // 导航栏
-    nav: [],
-    // 侧边栏
-    sidebar: {
-      "/": [
-        "",
-        "part1.md",
-        "ch1.md",
-        "ch2.md",
-        "part2.md",
-        "ch3.md",
-        "ch4.md",
-        "ch5.md",
-        "ch6.md",
-        "part3.md",
-        "ch7.md",
-        "ch8.md",
-        "ch9.md",
-        "ch10.md",
-        "ch11.md",
-        "part4.md",
-        "ch12.md",
-        "ch13.md",
-        "ch14.md",
-        "part5.md",
-        "ch15.md",
-        "ch16.md",
-        "ch17.md",
-        "ch18.md",
-        "ch19.md",
-        "ch20.md",
-        "ch21.md",
-        "ch22.md",
-        "ch23.md",
-        "ch24.md",
-        "ch25.md",
-        "ch26.md",
-        "ch27.md",
-        "ch28.md",
-        "ch29.md",
-        "part6.md",
-        "ch30.md",
-        "ch31.md",
-        "ch32.md",
-        "ch33.md",
-        "ch34.md",
-        "afterword.md"
-      ]
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Clean Architecture"
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "架构整洁之道"
     }
+  },
+  themeConfig: {
+    locales: {
+      "/": {
+        label: "English",
+        selectText: "Languages",
+        ariaLabel: "Select language",
+        editLinkText: "Edit this page on GitHub",
+        lastUpdated: "Last Updated",
+        nav: [],
+        sidebar: {
+          "/": [
+            "",
+            "part1.md",
+            "ch1.md",
+            "ch2.md",
+            "part2.md",
+            "ch3.md",
+            "ch4.md",
+            "ch5.md",
+            "ch6.md",
+            "part3.md",
+            "ch7.md",
+            "ch8.md",
+            "ch9.md",
+            "ch10.md",
+            "ch11.md",
+            "part4.md",
+            "ch12.md",
+            "ch13.md",
+            "ch14.md",
+            "part5.md",
+            "ch15.md",
+            "ch16.md",
+            "ch17.md",
+            "ch18.md",
+            "ch19.md",
+            "ch20.md",
+            "ch21.md",
+            "ch22.md",
+            "ch23.md",
+            "ch24.md",
+            "ch25.md",
+            "ch26.md",
+            "ch27.md",
+            "ch28.md",
+            "ch29.md",
+            "part6.md",
+            "ch30.md",
+            "ch31.md",
+            "ch32.md",
+            "ch33.md",
+            "ch34.md",
+            "afterword.md"
+          ]
+        }
+      },
+      "/zh/": {
+        label: "简体中文",
+        selectText: "选择语言",
+        ariaLabel: "选择语言",
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdated: "上次更新",
+        nav: [],
+        sidebar: {
+          "/zh/": [
+            "",
+            "part1.md",
+            "ch1.md",
+            "ch2.md",
+            "part2.md",
+            "ch3.md",
+            "ch4.md",
+            "ch5.md",
+            "ch6.md",
+            "part3.md",
+            "ch7.md",
+            "ch8.md",
+            "ch9.md",
+            "ch10.md",
+            "ch11.md",
+            "part4.md",
+            "ch12.md",
+            "ch13.md",
+            // "ch14.md",
+            // "part5.md",
+            // "ch15.md",
+            "ch16.md",
+            // "ch17.md",
+            // "ch18.md",
+            // "ch19.md",
+            // "ch20.md",
+            "ch21.md",
+            "ch22.md",
+            "ch23.md",
+            "ch24.md",
+            // "ch25.md",
+            // "ch26.md",
+            // "ch27.md",
+            // "ch28.md",
+            // "ch29.md",
+            // "part6.md",
+            // "ch30.md",
+            // "ch31.md",
+            // "ch32.md",
+            // "ch33.md",
+            // "ch34.md",
+            "afterword.md"
+          ]
+        }
+      }
+    },
+    repo: "gdut-yy/Clean-Architecture-zh",
+    repoLabel: "Github",
+    docsRepo: "gdut-yy/Clean-Architecture-zh",
+    docsBranch: "master/docs",
+    editLinks: true,
+    sidebarDepth: 2
   }
 };
